e1ad2e58395a3e6c794755363f71b01862ead2c1
Christof Kälin <christof.kaelin@protonmail.com>
2024-08-14 15:52:15 +0200

    logging h3 & h3

--- src/helpers/file-helpers.ts
+++ src/helpers/file-helpers.ts
@@ -1,46 +1,51 @@
 /* eslint-disable no-await-in-loop */
 /* eslint-disable no-restricted-syntax */
-import fs from "fs/promises";
-import matter from "gray-matter";
-import path from "path";
-import React from "react";
+import fs from "fs/promises"
+import matter from "gray-matter"
+import path from "path"
+import React from "react"
 
-const BLOG_POST_DIR_PATH = "/content";
+const BLOG_POST_DIR_PATH = "/content"
 
 export type BlogPostMetadata = {
-  title: string;
-  slug: string;
-};
+  title: string
+  slug: string
+}
 
 export const loadBlogPost = React.cache(async (slug: string) => {
-  const rawContent = await readFile(`${BLOG_POST_DIR_PATH}/${slug}.mdx`);
-  const { data: frontmatter, content } = matter(rawContent);
+  const rawContent = await readFile(`${BLOG_POST_DIR_PATH}/${slug}.mdx`)
+  const { data: frontmatter, content } = matter(rawContent)
 
-  return { frontmatter, content };
-});
+  const h2 = content.match(/^##\s.+/gm) || []
+  const h3 = content.match(/^###\s.+/gm) || []
+  console.log(`h2: \n${h2}`)
+  console.log(`h3: \n${h3}`)
+
+  return { frontmatter, content }
+})
 
 export const getBlogPostList = async () => {
-  const fileNames = await readDirectory(BLOG_POST_DIR_PATH);
+  const fileNames = await readDirectory(BLOG_POST_DIR_PATH)
 
-  const blogPosts = [];
+  const blogPosts = []
 
   for (const fileName of fileNames) {
-    const rawContent = await readFile(`${BLOG_POST_DIR_PATH}/${fileName}`);
-    const { data: frontmatter } = matter(rawContent);
+    const rawContent = await readFile(`${BLOG_POST_DIR_PATH}/${fileName}`)
+    const { data: frontmatter } = matter(rawContent)
 
     blogPosts.push({
       slug: fileName.replace(".mdx", ""),
       title: frontmatter.title,
-    });
+    })
   }
 
-  return blogPosts;
-};
+  return blogPosts
+}
 
 function readFile(localPath: string) {
-  return fs.readFile(path.join(process.cwd(), localPath), "utf8");
+  return fs.readFile(path.join(process.cwd(), localPath), "utf8")
 }
 
 function readDirectory(localPath: string) {
-  return fs.readdir(path.join(process.cwd(), localPath));
+  return fs.readdir(path.join(process.cwd(), localPath))
 }
